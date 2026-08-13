6099790b42d196573393e41b37b07a4905913dee
Hardik Rajan Gadekar <hardikgadekar@gmail.com>
2026-08-13 12:20:39 +0530

    Update employee lists by removing 'Siddhesh'
    Removed 'Siddhesh' from the SALES_EMPLOYEES list and its email mapping.

--- Working_Report_Editor/config.py
+++ Working_Report_Editor/config.py
@@ -32,7 +32,7 @@ SALES_EMPLOYEES = [
     "Apoorva", "Abhijit", "Sakib", "Jayesh",
     "Saif", "Rajesh", "Manasvi", "Praful", "Sachin", "Aishwary",
     "Supriya", "Sayli", "Dhanshree", "Muskan", "Ellias", "Faisal",
-    "Akshata", "Siddhesh", "Chaitrali", "Ahleem", "Farhatnaaz", "Isha", "Simran",
+    "Akshata", "Chaitrali", "Ahleem", "Farhatnaaz", "Isha", "Simran",
 ]
 
 HR_EMPLOYEES = [
@@ -60,7 +60,6 @@ SALES_EMAIL_MAP = {
     "ellias.edujam@gmail.com": "Ellias", 
     "faisalk.edujam@gmail.com": "Faisal",
     "akshata.edujam@gmail.com": "Akshata",
-    "siddheshg.edujam@gmail.com": "Siddhesh",
     "chaitrali.edujam@gmail.com": "Chaitrali",
     "ahleem.edujam@gmail.com": "Ahleem",
     "farhatnaaz.edujam@gmail.com": "Farhatnaaz",
